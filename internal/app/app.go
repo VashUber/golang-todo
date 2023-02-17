c8279628cadaf0149b670940b49dd8c47fea6e50
VashUber <71492611+VashUber@users.noreply.github.com>
2023-02-17 17:33:09 +0300

    ¯\_(ツ)_/¯

--- internal/app/app.go
+++ internal/app/app.go
@@ -7,12 +7,32 @@ import (
 	"github.com/VashUber/golang-todo/infrastructure"
 )
 
+func AddTODO(input *widget.Entry, root *widget.Box) func() {
+	return func() {
+		lbl := widget.NewLabel(input.Text)
+		input.SetText("")
+
+		root.Append(lbl)
+	}
+}
+
 func Run(cfg *infrastructure.Config) {
 	a := app.New()
-	w := a.NewWindow("Hello World")
-
+	w := a.NewWindow("Todo")
 	w.Resize(fyne.NewSize(cfg.Size.W, cfg.Size.H))
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	input := widget.NewEntry()
+
+	content := &widget.Box{
+		Children: []fyne.CanvasObject{
+			input,
+		},
+	}
+
+	addTodo := AddTODO(input, content)
+	btn := widget.NewButton("Click", addTodo)
+	content.Append(btn)
+
+	w.SetContent(content)
 	w.ShowAndRun()
 }
